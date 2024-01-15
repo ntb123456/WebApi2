d00ee8c45380f4bd922a10a052251b8ce6f3fc0d
ntb123456 <narendrabahekar@gmail.com>
2024-01-15 14:15:25 +0530

    ok

---
+++ MultiplyWebAPI/Controllers/HomeadvanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiplyWebAPI.Controllers
+{
+    public class HomeadvanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
